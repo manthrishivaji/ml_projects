533547b139d3a975d95712c52607e24ff32d2a72
shivajimanthri <manthrishivaji13579@gmail.com>
2024-08-01 13:24:35 +0530

    notebook

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,5 @@
 import sys
-import logging
-from logger import logging
+from src.logger import logging
 
 
 
@@ -23,3 +22,9 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
 
+if __name__ == "__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Divided by zero")
+        raise CustomException(e,sys)
