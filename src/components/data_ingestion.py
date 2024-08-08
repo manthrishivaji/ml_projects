f3649caadf46d5a462835a37e8ceaf3255e6f2ba
shivajimanthri <manthrishivaji13579@gmail.com>
2024-08-08 13:07:55 +0530

    model trained with best params

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,8 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 
 @dataclass # if only defining variables use dataclass decorator
@@ -37,10 +39,10 @@ class DataIngestion:
   
             logging.info("Ingestion of the data is completed")
 
-            return{
+            return(
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
-            }
+            )
         except Exception as e:
             raise CustomException(e,sys)
         
@@ -50,4 +52,7 @@ if __name__=="__main__":
     train_data,test_data=obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer= ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
